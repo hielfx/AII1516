e5e61705e45e53b3fbe5c920e084a65bff9f6f80
Daniel Sánchez <dansanav@gmail.com>
2016-01-10 13:04:51 +0100

    Añadido atributos a la clase juego

--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,4 @@
+#encoding: utf-8
 from django.db import models
 
 # Create your models here.
@@ -6,4 +7,7 @@ class Juego(models.Model):
     url = models.URLField()
     nombre = models.CharField(max_length=255)
     precio = models.FloatField()
-
+    web = models.CharField(max_length=255)#Página web de la que procede
+    
+    def __str__(self):
+        return str(self.nombre)+" - "+str(self.precio)
